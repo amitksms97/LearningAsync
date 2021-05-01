ac0f3d3b29a1230bf200abbf7bcdf58f5d41b33c
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:25:29 +0530

    Add project files.

---
+++ LearningAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            Download();
+            Console.ReadLine(); //To type 
+        }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Downloading Complete");
+            });
+            }
+    }
+}
